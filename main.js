c93fc4ecd26e60b7779c7f0e2b6489b7da1d5d55
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-19 06:56:06 +0500

    75% dome

--- main.js
+++ main.js
@@ -3,24 +3,50 @@ import { product } from "./data.js";
 const productBox = document.querySelector(".products_box");
 const cartBox = document.querySelector(".cart_box");
 
+let cart = [];
 
-
-
-
-const renderProduct = () => {
-  productBox.innerHTML = product.map(
-    (item) => `
-    <div class="product">
+const renderProduct = (props) => {
+  if (props == "product") {
+    return (productBox.innerHTML = product
+      .map(
+        (item) => `
+          <div class="product">
+              <div class="img_block">
+                  <img class="product_img" width="200" src="${item.img}" alt="">
+               </div>
+                   <h2 class="product_title">${item.name}</h2>
+                   <h3 class="product_price">$ ${item.price}</h3>
+                  <button data-product ="${item.id}">add</button>
+          </div> 
+          `
+      )
+      .join());
+  }
+  cartBox.innerHTML = cart.map((item) => 
+    `    <div class="product">
         <div class="img_block">
-            <img class="product_img" width="200" src="./img/product1.jpg" alt="">
+            <img class="product_img" width="200" src="${item.img}" alt="">
          </div>
-             <h2 class="product_title">title</h2>
-             <h3 class="product_price">$</h3>
-            <button class="btn_add">add</button>
-    </div> 
-    `
-  ).join()
+             <h2 class="product_title">${item.name}</h2>
+             <h3 class="userPrice">$ ${item.price}</h3>
+             <button >-</button>
+             <span>${item.count}</span>
+             <button >+</button>
+    </div> `
+  );
 };
-renderProduct();
+renderProduct("product");
 
+productBox.addEventListener("click", (e) => {
+  const { dataset } = e.target;
+  const prdoduct = cart.find((p) => p.id == dataset.product )
+  if (!prdoduct){
+      for (let i of product) {
+        if (i.id == dataset.product) {
+          cart.push({ ...i, count: 1, userPrice: i.price });
+        }
+      }
 
+    }
+    renderProduct("cart");
+});
